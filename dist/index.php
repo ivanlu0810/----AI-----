1afd5d6b16f09846f3832cae20f682ed85c4ff58
108534156 <108534156@stu.ukn.edu.tw>
2025-08-07 17:12:47 +0800

    手臂

--- dist/index.php
+++ dist/index.php
@@ -72,51 +72,24 @@ $email = $_SESSION['email'] ?? 'No email';
 
                         <li class="sidebar-item  has-sub">
                             <a href="#" class='sidebar-link'>
-                                <i class="bi bi-stack"></i>
-                                <span>Components</span>
+                                <i class="bi bi-activity"></i>
+                                <span>訓練部位</span>
                             </a>
                             <ul class="submenu ">
                                 <li class="submenu-item ">
-                                    <a href="component-alert.html">Alert</a>
+                                    <a href="training-arms.html">手臂</a>
                                 </li>
                                 <li class="submenu-item ">
-                                    <a href="component-badge.html">Badge</a>
+                                    <a href="training-chest.html">胸部</a>
                                 </li>
                                 <li class="submenu-item ">
-                                    <a href="component-breadcrumb.html">Breadcrumb</a>
+                                    <a href="training-back.html">背部</a>
                                 </li>
                                 <li class="submenu-item ">
-                                    <a href="component-button.html">Button</a>
+                                    <a href="training-legs.html">腿</a>
                                 </li>
                                 <li class="submenu-item ">
-                                    <a href="component-card.html">Card</a>
-                                </li>
-                                <li class="submenu-item ">
-                                    <a href="component-carousel.html">Carousel</a>
-                                </li>
-                                <li class="submenu-item ">
-                                    <a href="component-dropdown.html">Dropdown</a>
-                                </li>
-                                <li class="submenu-item ">
-                                    <a href="component-list-group.html">List Group</a>
-                                </li>
-                                <li class="submenu-item ">
-                                    <a href="component-modal.html">Modal</a>
-                                </li>
-                                <li class="submenu-item ">
-                                    <a href="component-navs.html">Navs</a>
-                                </li>
-                                <li class="submenu-item ">
-                                    <a href="component-pagination.html">Pagination</a>
-                                </li>
-                                <li class="submenu-item ">
-                                    <a href="component-progress.html">Progress</a>
-                                </li>
-                                <li class="submenu-item ">
-                                    <a href="component-spinner.html">Spinner</a>
-                                </li>
-                                <li class="submenu-item ">
-                                    <a href="component-tooltip.html">Tooltip</a>
+                                    <a href="training-abs.html">腹部</a>
                                 </li>
                             </ul>
                         </li>
